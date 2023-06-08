5c428ecb67c072064e39659641f943d14b6bb4a9
XenophonLXH <lourenzo321@gmail.com>
2023-06-08 21:50:27 +0200

    [IMP] styling,script: Started with carousal for specials and added hover effect on navbar menuitems

---
+++ static/js/script.js
@@ -0,0 +1,37 @@
+// Carousal
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function nextSlide(n) {
+    showSlides(slideIndex + 1);
+}
+
+function currentSlide(n) {
+    showSlides(n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    // Reset to 1 if
+    // last slide
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    // Reset to last
+    // slide if navigating before first
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    // Update the dots
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
